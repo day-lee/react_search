9b99b8e876088a46f41743bfca2b3c43ed8baac3
Dayeon Lee <iamdayeonlee@gmail.com>
2024-07-31 14:41:12 +0100

    Add search and reset features

---
+++ src/data.js
@@ -0,0 +1,52 @@
+const data = [
+  {
+    id: "0",
+    title: "React",
+    author: "React Author",
+    url: "https://reactjs.org",
+    comments: 3,
+    point: 5,
+  },
+  {
+    id: "1",
+    title: "Redux",
+    author: "Redux Author",
+    url: "https://reduxjs.org",
+    comments: 2,
+    point: 4,
+  },
+  {
+    id: "2",
+    title: "Google",
+    author: "Google Author",
+    url: "https://google.com",
+    comments: 2,
+    point: 4,
+  },
+  {
+    id: "3",
+    title: "Python",
+    author: "Python Author",
+    url: "https://python.org",
+    comments: 1,
+    point: 9,
+  },
+  {
+    id: "4",
+    title: "Flask",
+    author: "Flask Author",
+    url: "https://Flask.org",
+    comments: 6,
+    point: 2,
+  },
+  {
+    id: "5",
+    title: "Django",
+    author: "Django Author",
+    url: "https://django.com",
+    comments: 8,
+    point: 1,
+  },
+];
+
+export default data;
